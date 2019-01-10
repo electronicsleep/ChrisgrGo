ef2351deb4722b403007d918b6a9bcbacf9516f2
Chris <electronicsleep@gmail.com>
2019-01-09 18:37:20 -0800

    endpoint test

--- endpoint_test.go
+++ endpoint_test.go
@@ -31,6 +31,34 @@ func TestHealthCheckHandler(t *testing.T) {
 	}
 }
 
+func TestEndpointsCheckHandler(t *testing.T) {
+	endpoint_list := []string{"/about", "/", "/linux", "/apple", "/projects"}
+	for _, endpoint := range endpoint_list {
+		fmt.Println("endpoint: ", endpoint)
+		req, err := http.NewRequest("GET", endpoint, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(templatePageHandler)
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: (%v : %v)", status, http.StatusOK)
+		} else {
+			fmt.Printf("handler returned correct status code: (%v : %v)\n", status, http.StatusOK)
+		}
+
+		expected := "linux"
+		if !strings.Contains(rr.Body.String(), expected) {
+			t.Errorf("handler did not find expected string body: (expected: %v, endpoint: %v)", expected, endpoint)
+		} else {
+			fmt.Printf("handler contained expected string: (expected: %v, endpoint: %v)\n", expected, endpoint)
+		}
+	}
+
+}
+
 func TestTimeHandler(t *testing.T) {
 	req, err := http.NewRequest("GET", "/time", nil)
 	if err != nil {
